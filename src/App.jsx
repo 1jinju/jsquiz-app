908bd2bbfa0d262ba06b16c3aa97d5ec2116bb29
1jinju <gkswlswn2044@gmail.com>
2023-06-14 23:54:37 +0900

    퀴즈 결과화면 표시, 점수 계산

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ const App = () => {
   const [quiz, setQuiz] = useState([]);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [isResultVisible, setIsResultVisible] = useState(false);
+
 
   useEffect(() => {
     const fetchQuizData = async () => {
@@ -19,32 +22,50 @@ const App = () => {
     fetchQuizData();
   }, []);
 
-  const checkAnswer = () => {
+  const checkAnswer = (selectedAnswerIndex) => {
+    const currentQuiz = quiz[currentIndex];
+
+    if (selectedAnswerIndex === currentQuiz.correct) {
+      setScore(score + 1);
+    }
     if (currentIndex < quiz.length - 1) {
       setCurrentIndex(currentIndex + 1);
+    }else {
+      setIsResultVisible(true);
     }
+
   };
 
   if (quiz.length === 0) {
     return <p>Loading...</p>;
   }
 
+  if (!isResultVisible) {
+    return (
+      <div>
+        <p>{quiz[currentIndex].question}</p>
+        {quiz[currentIndex].answers.map((answer, index) => (
+          <div key={index}>
+            <button
+              className={selectedAnswer === index ? 'selected' : ''} // 선택된 버튼에 클래스 추가
+              onClick={() => setSelectedAnswer(index)}
+            >
+              {answer}
+            </button>
+          </div>
+        ))}
+        <br/>
+        <button disabled={selectedAnswer === null} onClick={() => checkAnswer(selectedAnswer)}>
+          다음 문제로
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <p>{quiz[currentIndex].question}</p>
-      {quiz[currentIndex].answers.map((answer, index) => (
-        <div key={index}>
-          <button
-            className={selectedAnswer === index ? 'selected' : ''}
-            onClick={() => setSelectedAnswer(index)}
-          >
-            {answer}
-          </button>
-        </div>
-      ))}
-      <button disabled={selectedAnswer === null} onClick={() => checkAnswer()}>
-        다음 문제
-      </button>
+      <h2>퀴즈 결과</h2>
+      <p>점수: {score}/10</p>
     </div>
   );
 };
